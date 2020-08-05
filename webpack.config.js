4e84ddef4c1e776712220e358c4a168e88074b18
aleksey-mu <pacuklex@mail.ru>
2020-08-05 18:40:08 +0300

    fix: index.tsx path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: { index: './src/js/index.js' },
+	entry: { index: './src/index.tsx' },
 	output: {
 		path: path.join(__dirname, '/dist'),
 		filename: 'bundle.js',
